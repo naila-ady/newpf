d60bb22ff484617d6d6b7de08b5340bd7432dd32
naila-ady <164941279+naila-ady@users.noreply.github.com>
2024-11-11 20:19:29 +0500

    .

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -28,44 +28,40 @@ const Project=() =>{
                Amazon clone based on only HTML5 & CSS3.
                A Project fully Responsive & Attractive. 
           </p>
-          <Link href={"https://amazon-com-one.vercel.app/"} className="mt-3 text-indigo-500 inline-flex items-center">
+          <Link href={"https://amazon-com-one.vercel.app/"}
+           className="mt-3 text-indigo-500 inline-flex items-center">
           View Project
-           
-          </Link>
+           </Link>
         </div>
+      </div>
       </div>
       <div className="flex flex-wrap -mx-2">
         <div className="px-2 w-1/2">
           <div className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-            
-          <Image
+            <Image
           src={img2}
-          alt="Ststic-Resume"
+          alt="Static-Resume"
           width={100}
           height={100}
-          className="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
-          
-            />
+          className="w-full object-cover h-full object-center block opacity-25 absolute inset-0"/>
             <div className="text-center relative z-10 w-full">
               <h2 className="text-3xl text-[#003152] font-semibold title-font mb-2">
                   STATIC RESUME
               </h2>
-              <p className="leading-relaxed">
+              <p className="leading-relaxed text-[#d28882]">
                  An Static Resume with Dyanmic pages,
                   </p>
               <Link href={"https://static-resume-9xb1dlyoy-nailaadys-projects.vercel.app"} 
               className="mt-3 text-indigo-500 inline-flex items-center">
                 View Project
-                
-              </Link>
+               </Link>
             </div>
           </div>
-        </div>
-        <div className="px-2 w-1/2">
+           <div className="px-2 w-1/2">
           <div className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
             <Image
               src={img3}
-              alt="gallery"
+              alt="govrner website clone"
               width={100}
               height={100}
               className="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
@@ -74,13 +70,13 @@ const Project=() =>{
               <h2 className="text-3xl text-[#003152] font-semibold title-font mb-2">
               GIAIC WEBSITE 
               </h2>
-              <p className="leading-relaxed">
-               Clone of GIAIC website  using Next.justify And React Components.
-
-              </p>
-              <Link href={"#"} className="mt-3 text-indigo-500 inline-flex items-center">
-                View Project
-                </Link>
+              <p className="leading-relaxed  text-[#d28882]">
+               Clone of GIAIC website  using Next.JS And React Components.
+               </p>
+               <Link href={"https://amazon-com-one.vercel.app/"}
+               className="mt-3 text-indigo-500 inline-flex items-center">
+                 View Project
+              </Link>
             </div>
           </div>
         </div>
