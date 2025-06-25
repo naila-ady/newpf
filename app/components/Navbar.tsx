927c17cff1dea99a2db7c61c4ebae2c9c83ad9b6
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-06-26 01:30:16 +0500

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -26,19 +26,18 @@ import { IoCloudDownloadOutline } from "react-icons/io5";
         <Link href={"#Contact"} className="mr-5  text-[#d28882]  text-1xl hover:text-[#003152]">
         Contact</Link>
       </nav>
-     <a 
-  href="https://www.linkedin.com/in/your-username" 
-  target="_blank" 
-  rel="noopener noreferrer"
+     <Link
+  href={"https://portfoliocssnxtjs.vercel.app/ "}
+ 
 >
       <button className="inline-flex items-center text-[#d28882] bg-[#74bedb]  border-0 py-1 px-3 mx-0
       focus:outline-none hover:bg- rounded text-base mt-4 md:mt-0 gap-1">
        
-        Linked_in
+        MY_Resume
         <div className="text-2xl text-[#d28882] ">
        <IoCloudDownloadOutline/></div>
        </button>
-       </a>
+       </Link>
     </div>
   </header>
   </div>
